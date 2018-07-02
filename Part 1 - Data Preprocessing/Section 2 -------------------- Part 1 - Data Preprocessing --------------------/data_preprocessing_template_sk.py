8ed773d58e641680b5d8b9c9f4df0ceb28f3c24b
Kumar <sobhan.kumar@accenture.com>
2018-07-02 12:23:46 +0530

    Practice 02/7/18 Split data into test datasets

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template_sk.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template_sk.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 
 # Taking care of missing data
@@ -25,6 +25,19 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 #Encoding catagorical Data
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0]) 
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_X.fit_transform(y)
+
+
+
+# Splitting the data set as training set and test set 
+
+from sklearn.cross_validation import train_test_split
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2, random_state = 0)
+
